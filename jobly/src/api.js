124822e8c422744aa351c51ddc1aa72e7a4038df
Joyce <hellojoycewong@gmail.com>
2021-07-26 16:40:34 -0700

    Load list of companies from api to component

--- jobly/src/api.js
+++ jobly/src/api.js
@@ -36,8 +36,14 @@ class JoblyApi {
 
   // Individual API routes
 
-  /** Get details on a company by handle. */
+  /**Get list of companies */
+  static async getCompanies() {
+    let res = await this.request(`companies/`);
+    return res.companies;
+  }
 
+
+  /** Get details on a company by handle. */
   static async getCompany(handle) {
     let res = await this.request(`companies/${handle}`);
     return res.company;
